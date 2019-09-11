08aaf73efe745e5ccf32946d798bf9a3036f900a
dder725 <dder725@aucklanduni.ac.nz>
2019-09-11 15:50:06 +1200

    Added an early version of the concert class

--- se325-concert-service/src/main/java/se325/assignment01/concert/service/domain/Concert.java
+++ se325-concert-service/src/main/java/se325/assignment01/concert/service/domain/Concert.java
@@ -1,20 +1,76 @@
 package se325.assignment01.concert.service.domain;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import javax.persistence.*;
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
+@Entity
+@Table(name="CONCERTS")
 public class Concert {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    // TODO Implement this class. test
+    private String title;
+
+    private String imageName;
+
+    private String blurb;
+
+    @ElementCollection
+    @Column(name = "DATES")
+    @CollectionTable(name = "CONCERT_DATES")
+    private List<LocalDateTime> dates = new ArrayList<>();
+
+    @ElementCollection
+    @Column(name= "PERFORMERS")
+    @ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinTable(name = "CONCERT_PERFORMER",
+            joinColumns = @JoinColumn(name = "CONCERT_ID"),
+            inverseJoinColumns = @JoinColumn(name = "PERFORMER_ID")
+    )
+    private List<Performer> performers = new ArrayList<>();
+
+
+    // TODO Implement this class.
+    //Default constructor required by JPA
+    protected Concert(){}
+
+    public Concert(Long id, String title, String imageName, List<LocalDateTime> dates, List<Performer> performers){
+        this.id = id;
+        this.title = title;
+        this.dates = dates;
+        this.performers = performers;
+    }
 
     public Set<LocalDateTime> getDates() {
         return null;
     }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public String getBlurb() {
+        return blurb;
+    }
+
+    public List<Performer> getPerformers() {
+        return performers;
+    }
+
 }
